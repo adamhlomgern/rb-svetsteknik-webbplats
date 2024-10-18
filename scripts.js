ce23bc03b9aba95ac2d385d0235675a7e5c10154
adamhlomgern <146996877+adamhlomgern@users.noreply.github.com>
2024-10-18 16:24:15 +0200

    tag

--- scripts.js
+++ scripts.js
@@ -18,10 +18,11 @@ function ShowHide(element) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', GOOGLE_PROPERTY_ID_GOES_HERE, 'auto');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'GTM-MS67TZ8B', 'auto');
 ga('send', 'pageview');
 </script>
+
 
 
 var swiper = new Swiper('.mySwiper', {
